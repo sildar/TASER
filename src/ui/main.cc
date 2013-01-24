1adb697f7d1b00fbd1846c1fe291671dd294426a
sildar <sildar44@gmail.com>
2013-01-24 17:25:58 +0100

    Added examples for deleting a subtask and checking a task.

--- src/ui/main.cc
+++ src/ui/main.cc
@@ -5,10 +5,21 @@ int main(int arc, char** argv)
 {
   //creates a parent and its child and print them.
   Task* parent = new Task("parent", NULL, true);
-  Task son("son", parent, false);
+  Task* son = new Task("son", parent, false);
   std::cout << "Hello, QtWorld !" << std::endl;
-  std::cout << son.hasParent() << std::endl;
+
   std::cout << parent->toString() << std::endl;
+
+
+  //deletes a subtask
+  parent->deleteSubtask(son);
+  std::cout << parent->toString() << std::endl;
+
+  //checks a task
+
+  parent->checkTask();
+  std::cout << parent->toString() << std::endl;
+  
 
   return 0;
 }
